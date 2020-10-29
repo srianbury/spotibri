f2bcd06d708c856680d22c5deb67100177fb7e84
Brian Sunbury <bsunbury29@gmail.com>
2020-10-28 19:18:08 -0700

    bootstrap

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 import React from "react";
 import loadable from "@loadable/component";
-import "./src/styles/index.css";
-import "./src/styles/pure-min.css";
+// import "./src/styles/index.css";
+// import "./src/styles/pure-min.css";
+import "./src/styles/bootstrap.css";
 
 const Authentication = loadable(() =>
   import("./src/components/authentication")
